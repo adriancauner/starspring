6893da6efa273e3dfd8b27ccd7a3a7689941420e
adriancauner <adriannsamuelcauner@gmail.com>
2023-05-05 15:14:29 +0000

    aplication

---
+++ app/src/main/webapp/java/STARSPRING/WebApplication.java
@@ -0,0 +1,9 @@
+package startspring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[ ])
+    SprinApplication.run(WebApplication.class,args);
+}
